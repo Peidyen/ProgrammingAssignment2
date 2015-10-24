dd99a9b3a828c0dd1c49a9c270e6a81d96cdba2a
Peidyen <mitch.ronco@gmail.com>
2015-10-24 12:56:47 -0400

    Update cachematrix.R
    makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Creates a makeCacheMatrix function which allows one to get, set, the matrix as well as get and set the ## inverse of the matrix via the solve function.  Allows for the caching of the matrix
 
+
+makeCacheMatrix <- function(x = matrix()){    
+  m <- NULL
+  set <- function(y){
+    x <<- y  
+#store matrix in cache  
+ m <<- NULL 
+  }
+  
+  #get matrix
+  get <- function() x 
+  
+  #set inverse matrix
+  setInverse <- function(solve) m<<- solve 
+  
+  #get inverse matrix
+  getInverse <- function() m 
+  
+  ## create list of functions, similar to the revealing module pattern in javascript
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)  
 }
 
-
-## Write a short comment describing this function
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve retrieves the inverse from the cache, otherwise it solves for the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+      
+      ## Create matrix, inverse of x
+      
+      m <- x$getInverse()                 
+	  #request cache
+	  #is there a cached value?
+      if(!is.null(m)){                    
+          message("getting cached data")  
+          return(m)                       # return cached value
+        }
+		
+		# get matrix 
+      data <- x$get()                     
+	  # solve the inverse
+      m <- solve(data, ...)               
+	  # set the inverse using the make cache function
+      x$setInverse(m)                     
+    
+	#evaluate m to return it to the variable data
+	  m
+	  }
